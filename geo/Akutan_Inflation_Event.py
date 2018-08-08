7621ec4d4c667e95a66c355563e467e50f0fadf6
Cody Rude <cmrude@mit.edu>
2018-08-08 15:35:48 -0400

    Updated for changes in skdiscovery

--- geo/Akutan_Inflation_Event.py
+++ geo/Akutan_Inflation_Event.py
@@ -10,15 +10,15 @@ from skdaccess.geo.grace import DataFetcher as GRACEDF
 from skdaccess.geo.gldas import DataFetcher as GLDASDF
 
 # Setup skdiscovery
-from skdiscovery import DiscoveryPipeline
-from skdiscovery.framework.stagecontainers import *
-import skdiscovery.table.filters
-import skdiscovery.table.analysis
-import skdiscovery.table.accumulators
-import skdiscovery.series.filters
-import skdiscovery.series.analysis
-import skdiscovery.series.accumulators
-import skdiscovery.generic.accumulators
+from skdiscovery.data_structure.framework import DiscoveryPipeline
+from skdiscovery.data_structure.framework.stagecontainers import *
+import skdiscovery.data_structure.table.filters
+import skdiscovery.data_structure.table.analysis
+import skdiscovery.data_structure.table.accumulators
+import skdiscovery.data_structure.series.filters
+import skdiscovery.data_structure.series.analysis
+import skdiscovery.data_structure.series.accumulators
+import skdiscovery.data_structure.generic.accumulators
 
 # Start and end date
 pbo_start_date = '2006-01-01'
@@ -43,7 +43,7 @@ ap_lon = AutoParam(-165.98555)
 ap_radius = AutoParam(15)
 
 # GeoLocation filter
-fl_geo = skdiscovery.table.filters.GeoLocationFilter('GeoFilter',[ap_lat, ap_lon, ap_radius])
+fl_geo = skdiscovery.data_structure.table.filters.GeoLocationFilter('GeoFilter',[ap_lat, ap_lon, ap_radius])
 # Stage container for filter
 sc_geo = StageContainer(fl_geo)
 
@@ -52,13 +52,13 @@ sc_geo = StageContainer(fl_geo)
 ap_label_list = AutoList(['AV13'])
 
 # Create table filter
-fl_table = skdiscovery.table.filters.TableFilter('TableFilter', [ap_label_list])
+fl_table = skdiscovery.data_structure.table.filters.TableFilter('TableFilter', [ap_label_list])
 
 # Create Stage Container
 sc_table = StageContainer(fl_table)
 
 # Create Stabilization Filter for stabilizing a region
-fl_stab = skdiscovery.table.filters.StabilizationFilter('StabFilter')
+fl_stab = skdiscovery.data_structure.table.filters.StabilizationFilter('StabFilter')
 # Stage container for filter
 sc_stab = StageContainer(fl_stab)
 
@@ -70,7 +70,7 @@ ap_kf_sigmasq = AutoParam(4)
 ap_kf_R = AutoParam(1)
 
 # Create Kalman Smoother
-fl_kf = skdiscovery.table.filters.KalmanFilter('Kalman Smoother', [ap_kf_tau, ap_kf_sigmasq, ap_kf_R])
+fl_kf = skdiscovery.data_structure.table.filters.KalmanFilter('Kalman Smoother', [ap_kf_tau, ap_kf_sigmasq, ap_kf_R])
 
 # Create stage container for low pass filter
 sc_kf = StageContainer(fl_kf)
@@ -86,7 +86,7 @@ ap_pca_type = AutoParam('PCA')
 pca_column_names = ('dN','dE')
 
 # Create PCA analysis item
-ana_gca = skdiscovery.table.analysis.General_Component_Analysis('PCA', [ap_pca_type, ap_pca_start, ap_pca_end],
+ana_gca = skdiscovery.data_structure.table.analysis.General_Component_Analysis('PCA', [ap_pca_type, ap_pca_start, ap_pca_end],
                                                             n_components=3, column_names=pca_column_names)
 # Create stagecontainer for PCA analysis                                                       
 sc_gca = StageContainer(ana_gca)
@@ -96,7 +96,7 @@ sc_gca = StageContainer(ana_gca)
 pca_name = 'PCA'
 
 # Create plotter item
-acc_gpsplot = skdiscovery.generic.accumulators.GPSHPlotter('GPSHPlotter',pca_name)
+acc_gpsplot = skdiscovery.data_structure.generic.accumulators.GPSHPlotter('GPSHPlotter',pca_name)
 
 # Create Stage Container
 sc_gpsplot = StageContainer(acc_gpsplot)
